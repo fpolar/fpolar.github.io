4fecb87bbaa1856d7d8db7927974e4a22533198a
Frank A Pol <fpol@usc.edu>
2020-10-13 19:48:29 -0700

    angular node and bootstrap setup

---
+++ hw8/stocks/src/main.ts
@@ -0,0 +1,16 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
