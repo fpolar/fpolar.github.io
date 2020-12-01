dc1f5f265a8c12e91692d353f479e5ae0929c9a9
Frank A Pol <fpol@usc.edu>
2020-11-30 19:48:45 -0800

    basic autocomplete fun incorporated into my search bar

---
+++ Stocks/app/src/main/java/com/android/stocks/AutoCompSearchView.java
@@ -0,0 +1,124 @@
+package com.android.stocks;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.AppCompatAutoCompleteTextView;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.view.ActionProvider;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AutoCompSearchView extends ActionProvider {
+    Context mContext;
+
+    private static final int TRIGGER_AUTO_COMPLETE = 100;
+    private static final long AUTO_COMPLETE_DELAY = 300;
+    private Handler handler;
+    private AutoSuggestAdapter autoSuggestAdapter;
+
+    public AutoCompSearchView(Context context) {
+        super(context);
+        mContext = context;
+    }
+
+    @Override
+    public View onCreateActionView() {
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+        View providerView =
+                layoutInflater.inflate(R.layout.auto_comp_search, null);
+
+            final AppCompatAutoCompleteTextView autoCompleteTextView =  providerView.findViewById(R.id.auto_complete_edit_text);
+            final TextView selectedText = providerView.findViewById(R.id.selected_item);
+
+            //Setting up the adapter for AutoSuggest
+            autoSuggestAdapter = new AutoSuggestAdapter(mContext, android.R.layout.simple_dropdown_item_1line);
+            autoCompleteTextView.setThreshold(2);
+            autoCompleteTextView.setAdapter(autoSuggestAdapter);
+            autoCompleteTextView.setOnItemClickListener(
+                    new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view,
+                                                int position, long id) {
+                            selectedText.setText(autoSuggestAdapter.getObject(position));
+                        }
+                    });
+            autoCompleteTextView.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int
+                        count, int after) {
+                }
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before,
+                                          int count) {
+                    handler.removeMessages(TRIGGER_AUTO_COMPLETE);
+                    handler.sendEmptyMessageDelayed(TRIGGER_AUTO_COMPLETE,
+                            AUTO_COMPLETE_DELAY);
+                    Log.d("CREATION", "onTextChanged: "+s);
+                }
+                @Override
+                public void afterTextChanged(Editable s) {
+                }
+            });
+            handler = new Handler(new Handler.Callback() {
+                @Override
+                public boolean handleMessage(Message msg) {
+                    if (msg.what == TRIGGER_AUTO_COMPLETE) {
+                        if (!TextUtils.isEmpty(autoCompleteTextView.getText())) {
+                            makeApiCall(autoCompleteTextView.getText().toString());
+                        }
+                    }
+                    return false;
+                }
+            });
+
+        return providerView;
+    }
+
+    private void makeApiCall(String text) {
+        ApiCall.make(mContext, text, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //parsing logic, please change it as per your requirement
+                List<String> stringList = new ArrayList<>();
+                try {
+                    JSONObject responseObject = new JSONObject(response);
+                    JSONArray array = responseObject.getJSONArray("results");
+                    for (int i = 0; i < array.length(); i++) {
+                        JSONObject row = array.getJSONObject(i);
+                        stringList.add(row.getString("trackName"));
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                //IMPORTANT: set data here and notify
+                autoSuggestAdapter.setData(stringList);
+                autoSuggestAdapter.notifyDataSetChanged();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+    }
+}
+
--- Stocks/app/src/main/java/com/android/stocks/MainActivity.java
+++ Stocks/app/src/main/java/com/android/stocks/MainActivity.java
@@ -1,12 +1,20 @@
 package com.android.stocks;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.appcompat.widget.AppCompatAutoCompleteTextView;
 import androidx.appcompat.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,60 +22,33 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     Toolbar mToolbar;
     ArrayAdapter<String> mAdapter;
     ListView mListView;
     TextView mEmptyView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTheme(R.style.SplashTheme);
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(mToolbar);
-
-        mListView = (ListView) findViewById(R.id.list);
-        mEmptyView = (TextView) findViewById(R.id.emptyView);
-
-        mAdapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.months_array));
-        mListView.setAdapter(mAdapter);
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this, adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        mListView.setEmptyView(mEmptyView);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.options_menu, menu);
-
-        MenuItem mSearch = menu.findItem(R.id.action_search);
-
-        SearchView mSearchView = (SearchView) mSearch.getActionView();
-        mSearchView.setQueryHint("Search");
-
-        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                mAdapter.getFilter().filter(newText);
-                return true;
-            }
-        });
-
         return super.onCreateOptionsMenu(menu);
     }
+
 }
